c25871591a5b2c48f83420d88e13ae4b3845becb
Jason-Mora <jason.mora16@gmail.com>
2025-09-14 09:09:59 +0200

    Footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      <p>© {new Date().getFullYear()} Jason Mora – Tutti i diritti riservati</p>
+    <footer className="bg-gray-900 text-white text-center py-6 mt-10 flex gap-2 justify-center items-center flex-col sm:flex-row">
+      <p>© {new Date().getFullYear()} Jason Mora</p>
+      <p className="hidden sm:block">–</p>
+      <p>Tutti i diritti riservati</p>
     </footer>
   );
 }
